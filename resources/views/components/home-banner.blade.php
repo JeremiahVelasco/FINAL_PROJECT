af4fcdff059e03f7b91f77ce60d4dbe5cd1043b9
Jeremiah Daligues Velasco <velascojeremiahd@gmail.com>
2024-05-01 16:58:56 +0800

    updated home-banner

--- resources/views/components/home-banner.blade.php
+++ resources/views/components/home-banner.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-gray-900">
     <div class="relative mx-auto md:mt-8">
         <div class="lg:max-w-4xl lg:mx-auto">
-            <img class="max-h-48 w-full border-2 rounded-lg object-cover"
+            <img class="max-h-48 w-full rounded-lg object-cover"
                 src="https://images.unsplash.com/photo-1593118247619-e2d6f056869e?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         </div>
     </div>
