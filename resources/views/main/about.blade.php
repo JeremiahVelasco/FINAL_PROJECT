90c7992bd431d5677eb0257bae5057a5a7800a92
rawil21 <rawilrueca@gmail.com>
2024-05-01 16:53:52 +0800

    updated about page

--- resources/views/main/about.blade.php
+++ resources/views/main/about.blade.php
@@ -1,13 +1,12 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="overflow-hidden bg-gray-900 py-24 sm:py-32">
+<div class="overflow-hidden bg-gray-900 py-24 sm:py-32 mt-32">
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
       <div class="lg:pr-8 lg:pt-4">
         <div class="lg:max-w-lg">
-          <h2 class="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
-          <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</p>
+          <p class="mt-2 text-3xl font-bold tracking-tight text-gray-300 sm:text-7xl">CubicWave gets you in your great gaming groove.</p>
           <p class="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
           <dl class="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
             <div class="relative pl-9">
