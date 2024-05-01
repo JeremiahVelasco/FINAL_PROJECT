b42b7c7d0d6ea24fb19a13eec66ca9810daab912
Jeremiah Daligues Velasco <velascojeremiahd@gmail.com>
2024-05-01 17:33:22 +0800

    updated tab icon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CubicWave</title>
+    <link rel="icon" href="/storage/assets/cubicwave_lightblue.svg">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
